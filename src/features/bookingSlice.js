cdd099afc6e819d5dd5520f541225fc028079bc6
Ryan-Hozy <ryanho.rhzy@gmail.com>
2024-08-12 11:17:02 +0800

    updated the base_url to render's url

--- src/features/bookingSlice.js
+++ src/features/bookingSlice.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const BASE_URL = "https://e10465e6-2c62-4400-932b-e3b715e420a2-00-a06eotzji59s.pike.replit.dev";
+const BASE_URL = "https://api-render-io.onrender.com";
 
 export const createBooking = createAsyncThunk(
   "booking/createBooking",
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -12,7 +12,7 @@ import { FaEdit } from "react-icons/fa";
 import Nav from '../components/Nav';
 import axios from 'axios';
 
-const BASE_URL = 'https://e10465e6-2c62-4400-932b-e3b715e420a2-00-a06eotzji59s.pike.replit.dev'; // Replace with your actual API base URL
+const BASE_URL = 'https://api-render-io.onrender.com'; // Replace with your actual API base URL
 
 export default function Profile() {
   const auth = getAuth();
